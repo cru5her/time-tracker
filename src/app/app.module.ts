39655867577d64b5b250876a5e32992382fdafbd
koop <jimmy.koop@adesso.de>
2016-10-19 16:54:13 +0200

    inital project setup and add datepicker component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { Datepicker } from './calendar/calendar.component';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    NgbModule.forRoot()
+  ],
+  declarations: [
+    AppComponent,
+    Datepicker
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/calendar/calendar.component.ts
@@ -0,0 +1,17 @@
+import {Component} from '@angular/core';
+import {NgbDateStruct} from '@ng-bootstrap/ng-bootstrap';
+
+const now = new Date();
+
+@Component({
+  selector: 'ngbd-datepicker-basic',
+  templateUrl: './calendar.component.html'
+})
+export class Datepicker {
+
+  model: NgbDateStruct;
+
+  selectToday() {
+    this.model = {year: now.getFullYear(), month: now.getMonth() + 1, day: now.getDate()};
+  }
+}
